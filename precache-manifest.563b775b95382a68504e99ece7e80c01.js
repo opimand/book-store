15196934ccfb7f3345e9b599e376c446ec8b3143
opimand <Opima359>
2020-05-19 19:19:43 +0300

    Updates

---
+++ precache-manifest.563b775b95382a68504e99ece7e80c01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3a99859fb2d6c39a99427daef34c99",
+    "url": "/book-store/index.html"
+  },
+  {
+    "revision": "1c038a18862484cd0d58",
+    "url": "/book-store/static/js/2.fcb527bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/book-store/static/js/2.fcb527bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2aa00c4e73dde3a2b7",
+    "url": "/book-store/static/js/main.b3f61426.chunk.js"
+  },
+  {
+    "revision": "33e8d9963dbc2b95eea5",
+    "url": "/book-store/static/js/runtime-main.8e288ba4.js"
+  },
+  {
+    "revision": "b64311529c2aa05fde2ab08a835df11a",
+    "url": "/book-store/static/media/nightMode.b6431152.svg"
+  },
+  {
+    "revision": "960ef455f1a0bf6712ead199ad2994d6",
+    "url": "/book-store/static/media/no-photo.960ef455.svg"
+  },
+  {
+    "revision": "62e57662f821ed05e00e3461e8a0a561",
+    "url": "/book-store/static/media/spinner.62e57662.svg"
+  }
+]);
